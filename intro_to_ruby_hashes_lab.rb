4bce11800beb2416669e0218e2b7b3d3c4dfb183
Taylor Kraft <taylorkraft16@gmail.com>
2019-10-27 23:57:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,11 @@
 def base_hash
- monopoly = :railroads = {}
+ monopoly = {:railroads = {}
+ }
 end
 
 def monopoly_with_second_tier
- monopoly = {:railroads => :pieces => 4}
+ monopoly = {:railroads => {:pieces => 4}
+ }
 end
 
 def monopoly_with_third_tier
