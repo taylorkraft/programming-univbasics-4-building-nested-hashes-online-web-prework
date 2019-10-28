fd03835bfc442c70d04f702cc006467f51805948
Taylor Kraft <taylorkraft16@gmail.com>
2019-10-28 00:28:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -50,10 +50,21 @@ def monopoly_with_fourth_tier
      :names => 
      {
      :reading_railroad => {
-       "mortgage_value"},
-     :pennsylvania_railroad => {"mortgage_value"},
-     :b_and_o_railroad => {"mortgage_value"},
-     :shortline_railroad => {"mortgage_value"}
+       "mortgage_value" = $100
+       
+     },
+     :pennsylvania_railroad => {
+       "mortgage_value" = $200
+       
+     },
+     :b_and_o_railroad => {
+       "mortgage_value" = $400
+       
+     },
+     :shortline_railroad => {
+       "mortgage_value" = $800
+       
+     }
      }
    }
  }
