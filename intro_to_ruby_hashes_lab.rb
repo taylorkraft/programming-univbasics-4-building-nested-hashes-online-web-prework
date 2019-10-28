08af25561cd1f45217385b4b07bcee51768278da
Taylor Kraft <taylorkraft16@gmail.com>
2019-10-28 00:05:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,12 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  
+   monopoly = {
+   :railroads => {
+     :pieces => 4
+     :rent_in_dollars =>
+   }
+ }
 end
 
 def monopoly_with_fourth_tier
